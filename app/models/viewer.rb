ec54a9fd68adda04222d133cb4e226234e17246a
meryldakin <meryl.k.dakin@gmail.com>
2017-07-21 11:59:47 -0400

    Revert "adding all files, new material mixed in. this has the potential solutions included but not the tests yet."
    This reverts commit e6b33c5b5b7c5d39c4a9813c73d52380298de298.

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,29 +1,11 @@
 class Viewer
   attr_accessor :first_name, :last_name
 
-  @@all = []
-
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-    self.class.all << self
+  def initialize(name)
+    @name = name
   end
 
-  def self.all
-    @@all
+  def full_name
+    "#{first_name} #{last_name}"
   end
-
-  def watchlist
-    Watchlist.all.select { |watchlist| watchlist.viewer == self }
-  end
-
-  def create_or_edit_rating(movie, rating)
-    movie_watch = watchlist.find { |w| w.movie == movie }
-    movie_watch.rating = rating
-  end
-
-  def add_to_watchlist(movie_array)
-    movie_array.each { |movie| Watchlist.new(movie, self) }
-  end
-
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,21 +3,5 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-#viewers
-meryl = Viewer.new("Meryl", "Dakin")
-nathan = Viewer.new("Nathan", "Johnson")
-#genres
-action = Genre.new("Action")
-romance = Genre.new("Romance")
-#movies
-baby_driver = Movie.new("Baby Driver", action)
-matrix = Movie.new("The Matrix", romance)
-big_sick = Movie.new("The Big Sick", romance)
-#ratings
-rating = Watchlist.new(baby_driver, meryl)
-rating2 = Watchlist.new(matrix, meryl)
-rating3 = Watchlist.new(baby_driver, nathan)
-rating4 = Watchlist.new(matrix, nathan)
-rating5 = Watchlist.new(big_sick, nathan)
 
 Pry.start
